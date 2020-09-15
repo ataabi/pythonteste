f7dec1451350ea3c25819e14a236de2cdc6aac55
ataabi <jhony.prf@gmail.com>
2020-09-15 15:45:06 -0300

    curso

--- Desafios/ex033.py
+++ Desafios/ex033.py
@@ -1,7 +1,14 @@
 #Faça um programa que leia tres numeros e mostre qual é o maior e qual e o menor
-print('Digite 3 Valores')
+print('Digite 3 Números')
 n1 = float(input('1° numero : '))
 n2 = float(input('2° numero : '))
 n3 = float(input('3° numero : '))
-deck = sorted([n1,n2,n3])
-print(f'O maior é {deck[2]} e o menor é {deck[0]}')
+if n3 < n2 < n1:
+    print(f'O Maior N° é {n3} e o menor é {n1}')
+elif n2 < n3 < n1:
+    print(f'O Maior N° é {n1} e o menor é {n2}')
+elif n3 < n1 < n2:
+    print(f'O Maior N° é {n2} e o menor é {n3}')
+
+#deck = sorted([n1,n2,n3])
+#print(f'O maior é {deck[2]:.2f} e o menor é {deck[0]:.2f}')
--- Desafios/ex035.py
+++ Desafios/ex035.py
@@ -1,2 +1,10 @@
 #Desenvola um programa que leia o comprimento de tres retas e diga ao usuario
 # se elas podem ou nao formar um triangulo.
+r1 = int(input('Digite o valor da 1° Reta. \n:'))
+r2 = int(input('Digite o Valor da 2° Reta. \n:'))
+r3 = int(input('Digite o valor da 3° Reta \n:'))
+
+if r1 + r2 > r3 and r2 + r3 > r1 and r3 + r1 > r2:
+    print('Essas Retas formam um Triangulo')
+else:
+    print('Essas Retas nao formam um Triangulo')
